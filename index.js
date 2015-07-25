68ffac41fad697e928805d7e3e4a7a30e7da3aea
Surume <mail@sururne.com>
2015-07-26 00:35:14 +0900

    Add index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var remote = require('remote');
+var fileUtil = remote.require('./lib/fileUtil');
+
+fileUtil.fetchReadmeList(function(err, matches) {
+    if(!err) document.write(matches.join());
+});
