ab5b83e14e97cec5c3af7f4e331d246749d098b1
Surume <mail@sururne.com>
2015-07-26 02:25:31 +0900

    Add menu

--- main.js
+++ main.js
@@ -2,10 +2,9 @@
 
 var app = require('app');
 var BrowserWindow = require('browser-window');
+var Menu = require('menu');
 
 require('crash-reporter').start();
-
-var mainWindow = null;
 
 app.on('window-all-closed', function() {
     if (process.platform != 'darwin')
@@ -13,12 +12,46 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
+    // メニューをアプリケーションに追加
+    Menu.setApplicationMenu(menu);
 
-    // ブラウザ(Chromium)の起動, 初期画面のロード
-    mainWindow = new BrowserWindow({width: 800, height: 600});
-    mainWindow.loadUrl('file://' + __dirname + '/index.html');
-
-    mainWindow.on('closed', function() {
-        mainWindow = null;
-    });
+    openWindow(process.cwd());
 });
+
+function openWindow (baseDir) {
+    var win = new BrowserWindow({width: 800, height: 600});
+    win.loadUrl('file://' + __dirname + '/index.html?baseDir=' + encodeURIComponent(baseDir));
+    win.on('closed', function () {
+        win = null;
+    });
+}
+
+// メニュー情報の作成
+var template = [
+      {
+          label: 'ReadUs',
+          submenu: [
+                    {label:'Quit',accelerator:'Command+Q', click: function () {app.quit();}}
+          ]
+      }, {
+          label: 'File',
+          submenu: [
+              {label: 'Open', accelerator: 'Command+O', click: function() {
+                  // 「ファイルを開く」ダイアログの呼び出し
+                  require('dialog').showOpenDialog({ properties: ['openDirectory']}, function (baseDir){
+                      if(baseDir && baseDir[0]) {
+                          openWindow(baseDir[0]);
+                      }
+                  });
+              }}
+          ]
+      }, {
+          label: 'View',
+          submenu: [
+              {label: 'Reload', accelerator: 'Command+R', click: function() { BrowserWindow.getFocusedWindow().reloadIgnoringCache(); } },
+              { label: 'Toggle DevTools', accelerator: 'Alt+Command+I', click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); } }
+          ]
+      }
+];
+
+var menu = Menu.buildFromTemplate(template);
