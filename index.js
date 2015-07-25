ac9a51fcd29a862a258d1b4a011631c6b9772777
Surume <mail@sururne.com>
2015-07-26 00:46:53 +0900

    Add `\n` rendering path

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ var remote = require('remote');
 var fileUtil = remote.require('./lib/fileUtil');
 
 fileUtil.fetchReadmeList(function(err, matches) {
-    if(!err) document.write(matches.join());
+    if(!err) document.write(matches.join("\n"));
 });
