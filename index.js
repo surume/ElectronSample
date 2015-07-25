8aea372a42b86ffc2b6abbc72654655ea545e3b1
Surume <mail@sururne.com>
2015-07-26 02:17:08 +0900

    Improve View

--- index.js
+++ index.js
@@ -2,7 +2,30 @@
 
 var remote = require('remote');
 var fileUtil = remote.require('./lib/fileUtil');
+var baseDir = process.cwd();
 
-fileUtil.fetchReadmeList(function(err, matches) {
-    if(!err) document.write(matches.join("\n"));
+var ngModule = angular.module('readUs', []);
+
+ngModule.controller('MainController', function ($scope) {
+    var main = this;
+
+    // README.mdの取得
+    main.getFile = function(file) {
+        main.fileText = fileUtil.getAsText(file.filepath);
+    };
+
+    fileUtil.fetchReadmeList(baseDir, function (err, fileList) {
+        if(err) console.error(err);
+        $scope.$apply(function () {
+            main.fileList = fileList;
+        });
+    });
+});
+
+ngModule.directive('mdPreview', function () {
+    return function ($scope, $elem, $attrs) {
+        $scope.$watch($attrs.mdPreview, function(source) {
+            $elem.html(marked(source));
+        });
+    };
 });
